1bc1d560c0dc66970b3180c0b4002d2e7e594b76
marvo-gift <105926625+marvo-gift@users.noreply.github.com>
2022-07-18 23:28:32 +0400

    made changes in the js

--- formvalidation.js
+++ formvalidation.js
@@ -10,29 +10,50 @@ function validationForm(){
         // username.innerHTML = "Please enter your name";
         username.focus();
         return false;
-    }else if(username.value !== ""){
-        username.style.border = "2px solid green";
-
     }
-
-    if (email.value == ""){
-        // alert("Please enter your email");
-        email.style.border ="2px solid red";
-    }else if(email.value !== ""){
-        email.style.border = "2px solid green";
+    else{
+        username.style.border ="2px solid green"; 
     }
-   
-    if(email.value.indexOf("@",0)<0 || email.value.indexOf(".",0)<0){
-        // alert('Please enter a valid email address')
+    if (email.value == "") {
+        //alert("Please enter your email");
+        email.style.border = "5px solid red";
         email.focus();
         return false;
-    }
-    
-    if(phone.value == "" || phone.value.length != 10 ){
+      }
+      else {
+        email.style.border = "5px solid green";
+        email.focus();
+      }
+
+      if (email.value.indexOf("@", 0) < 0 || email.value.indexOf(".", 0) < 0) {
+        //alert("Please enter a valid email address")
+        email.style.border = "5px solid red";
+        email.focus();
+        return true;
+      }
+      else {
+        email.style.border = "5px solid green";
+        email.focus();
+      }
+    //   if (password.value == "") {
+    //     //alert("Please enter your email");
+    //     password.style.border = "5px solid red";
+    //     password.focus();
+    //     return false;
+    //   }
+    //   else {
+    //     password.style.border = "5px solid green";
+    //     email.focus();
+    //   }
+
+      if (phone.value == "" || phone.value.length  != 10){
         // alert("Please enter a valid phone number");
-        username.focus();
+        phone.style.border = "2px solid red";
+        // phone.focus();
         return false;
-    }else if(phone.value !== ""){
-    phone.style.border = "2px solid green";
-}
+    }
+    else {
+        phone.style.border = "2px solid green";
+    }
+
 }
